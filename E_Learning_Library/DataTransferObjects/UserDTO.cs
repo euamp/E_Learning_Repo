9c07ee17bc75a9476f66383513d295d5848e0427
euamp <61351874+euamp@users.noreply.github.com>
2023-06-07 00:10:31 +0300

    Adding DTO Folder with UserDTO

---
+++ E_Learning_Library/DataTransferObjects/UserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning_Library.DataTransferObjects;
+
+public class UserDTO
+{
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string IsGraduated { get; set; } = null!;
+}
