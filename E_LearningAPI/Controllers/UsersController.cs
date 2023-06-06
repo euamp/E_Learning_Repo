13efabd669caad7e4d1c87de1114ee034382358c
euamp <61351874+euamp@users.noreply.github.com>
2023-06-07 00:13:59 +0300

    Finished All Endpoints of UsersController

--- E_LearningAPI/Controllers/UsersController.cs
+++ E_LearningAPI/Controllers/UsersController.cs
@@ -26,25 +26,109 @@ namespace E_LearningAPI.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            var user = _context.Users.SingleOrDefault(x => x.Id == id);
+
+            if (user == null)
+            {
+                return NotFound("User Not Found");
+            }
+            else
+            {
+                return Ok(user);
+            }
         }
 
         // POST api/Users
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post(UserDTO userDTO)
         {
+            var user1 = _context.Users
+                .Where(u => u.Username == userDTO.Username)
+                .FirstOrDefault();
+
+            var user2 = _context.Users
+                .Where(e => e.Email == userDTO.Email)
+                .FirstOrDefault();
+
+            if (user1 is not null)
+            {
+                return BadRequest();
+            }
+            else if (user2 is not null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                User user = new User();
+                user.Username = userDTO.Username;
+                user.Password = userDTO.Password;
+                user.Email = userDTO.Email;
+                user.Firstname = userDTO.Firstname;
+                user.Lastname = userDTO.Lastname;
+                user.RoleName = "User";
+                user.IsGraduated = userDTO.IsGraduated;
+
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return Ok(user);
+            }
         }
 
         // PUT api/Users/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, UserDTO userDTO)
         {
+            var user = _context.Users.Find(id);
+
+            if (user != null)
+            {
+                try
+                {
+                    _context.Users
+                    .Where(i => i.Id == id)
+                    .ExecuteUpdate(s => s
+                        .SetProperty(u => u.Username, u => userDTO.Username)
+                        .SetProperty(p => p.Password, p => userDTO.Password)
+                        .SetProperty(f => f.Firstname, f => userDTO.Firstname)
+                        .SetProperty(l => l.Lastname, f => userDTO.Lastname)
+                        .SetProperty(e => e.Email, f => userDTO.Email)
+                        .SetProperty(g => g.IsGraduated, g => userDTO.IsGraduated)
+                    );
+
+                    return Ok(userDTO);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return BadRequest();
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
 
         // DELETE api/Users/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                return NotFound("User Not Found");
+            }
+            else
+            {
+                _context.Users
+                    .Where(i => i.Id.Equals(id))
+                    .ExecuteDelete();
+
+                return Ok("Deleted Successfully");
+            }
         }
     }
 }
