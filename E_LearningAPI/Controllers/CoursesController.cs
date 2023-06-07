ad137e52738efd3519b76c0d42baac58025a786f
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-06-07 22:43:47 +0300

    Added Courses controller

---
+++ E_LearningAPI/Controllers/CoursesController.cs
@@ -0,0 +1,48 @@
+﻿using E_Learning_Library.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_LearningAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CoursesController : ControllerBase
+{
+    private readonly ELearningDbContext _context;
+
+    public CoursesController(ELearningDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Courses
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/Courses/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/Courses
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/Courses/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/Courses/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
