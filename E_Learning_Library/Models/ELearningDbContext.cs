00a9ab51e738258f4b5291b149b983d4a0e2bb45
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-06-09 00:27:28 +0300

    nothing

--- E_Learning_Library/Models/ELearningDbContext.cs
+++ E_Learning_Library/Models/ELearningDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace E_Learning_Library.Models;
 
@@ -30,7 +31,6 @@ public partial class ELearningDbContext : DbContext
     public virtual DbSet<UserProgress> UserProgresses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=E_LearningDB;Integrated Security=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
