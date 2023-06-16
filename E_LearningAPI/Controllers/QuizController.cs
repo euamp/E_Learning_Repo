79221cccba2340fcc6ce05d7a577ea8cbb0c6124
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-06-16 18:37:11 +0300

    changes + Questions controller

---
+++ E_LearningAPI/Controllers/QuizController.cs
@@ -0,0 +1,79 @@
+﻿using E_Learning_Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_LearningAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class QuizController : ControllerBase
+{
+    private readonly ELearningDbContext _context;
+
+    public QuizController(ELearningDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Quiz
+    [HttpGet]
+    public ActionResult<IEnumerable<Quiz>> Get()
+    {
+        var quizes = _context.Quizzes.ToList();
+        return Ok(quizes);
+    }
+
+    // GET api/Quiz/5
+    [HttpGet("{id}")]
+    public ActionResult<Quiz> Get(int id)
+    {
+        var quiz = _context.Quizzes.SingleOrDefault(x => x.QuizId == id);
+
+        if (quiz == null)
+        {
+            return NotFound("Quiz Not Found");
+        }
+        else
+        {
+            return Ok(quiz);
+        }
+    }
+
+    // POST api/Quiz
+    [HttpPost]
+    public ActionResult Post([FromBody] string value)
+    {
+        var course = _context.Courses.ToList();
+
+        if(course == null)
+        {
+            return BadRequest();
+        }
+    }
+
+    // PUT api/Quiz/5
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/Quiz/5
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var quiz = _context.Quizzes.Find(id);
+
+        if (quiz == null)
+        {
+            return NotFound("Quiz Not Found");
+        }
+        else
+        {
+            _context.Quizzes
+                .Where(i => i.QuizId.Equals(id))
+                .ExecuteDelete();
+
+            return Ok("Deleted Successfully");
+        }
+    }
+}
---
+++ E_Learning_Library/DataTransferObjects/QuizCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning_Library.DataTransferObjects;
+
+public class QuizCreateDTO
+{
+    public int CourseId { get; set; }
+
+    public string QuizName { get; set; } = null!;
+}
