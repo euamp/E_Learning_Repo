5197afe9bc4482abf2ceb837461a1468354877a5
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-06-19 19:36:34 +0300

    All changes so far

--- E_LearningWebApp/Program.cs
+++ E_LearningWebApp/Program.cs
@@ -1,4 +1,7 @@
+using Blazored.LocalStorage;
 using E_LearningWebApp;
+using E_LearningWebApp.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//----------------------------------------------------------------------------------------------------------------
+// My own Dependency Injection
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
+//----------------------------------------------------------------------------------------------------------------
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7216/") });
 
 await builder.Build().RunAsync();
