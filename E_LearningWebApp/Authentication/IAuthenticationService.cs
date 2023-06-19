2333ce3a178b3de1d190e6475c9686bd7be1552b
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-06-19 16:31:58 +0300

    Implemented Authentication in the front end

---
+++ E_LearningWebApp/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using E_LearningWebApp.Models;
+
+namespace E_LearningWebApp.Authentication;
+
+public interface IAuthenticationService
+{
+    Task<string> Login(LoginModel userForAuthentication);
+    Task Logout();
+    Task<string> Register(SignUpModel registerUserModel);
+}
---
+++ E_LearningWebApp/Models/SignUpModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace E_LearningWebApp.Models;
+
+public class SignUpModel
+{
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [DisplayName("Email address:")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [DisplayName("First Name:")]
+    public string Firstname { get; set; } = string.Empty;
+
+    [Required]
+    [DisplayName("Last Name:")]
+    public string Lastname { get; set; } = string.Empty;
+}
