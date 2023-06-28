6674bf5a28050a5a3b2bd80744707b01842854e4
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-06-28 16:28:26 +0300

    Changes

---
+++ E_LearningWebApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace E_LearningWebApp.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string RoleName { get; set; } = null!;
+
+    public string IsGraduated { get; set; } = null!;
+}
